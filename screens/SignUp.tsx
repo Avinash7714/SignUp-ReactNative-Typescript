192dbd482055b20d6ee5ee13e20f7895bde73b3e
Avinash M <59330319+Avinash7714@users.noreply.github.com>
2020-11-01 17:26:38 +0530

    Update SignUp.tsx

--- screens/SignUp.tsx
+++ screens/SignUp.tsx
@@ -59,7 +59,7 @@ export default function SignUp() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "whitesmoke",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
   },
